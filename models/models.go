958d579159406ece6c2496d897cd4141a7d6cfcf
Ali <ali@clickexpress.ae>
2025-10-01 04:36:32 -0400

    Fix Admin model - allow password in JSON binding

--- models/models.go
+++ models/models.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -36,7 +37,7 @@ type Admin struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Username  string    `json:"username" gorm:"unique;not null"`
 	Email     string    `json:"email" gorm:"unique;not null"`
-	Password  string    `json:"-" gorm:"not null"`
+	Password  string    `json:"password" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
